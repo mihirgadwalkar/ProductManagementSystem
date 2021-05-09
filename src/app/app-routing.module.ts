36934f98032577814c6e5b25264617e8f2d3ce7b
mihir <mihirgadwalkar@gmail.com>
2021-05-09 23:12:04 +0530

    routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+const routes: Routes = [
+  { path:'home',component:HomeComponent},
+  { path:'register',component:RegisterComponent},
+  { path:'login',component:LoginComponent},
+  { path:'contact-us',component:ContactUsComponent},
+  { path:'products',component:ProductsComponent},
+  { path:'', redirectTo:'/login',pathMatch:'full'}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
